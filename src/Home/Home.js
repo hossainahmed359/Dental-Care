e1878247f005e4911e12f2e3c48feb16a4cc24c3
hossainahmed359 <hossainahmed359@gmail.com>
2021-10-18 16:41:18 +0600

    banner location fixed

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import Slider from '../components/Slider/Slider';
 
-import banner1 from '../Images/Banner/banner1.png'
-import banner2 from '../Images/Banner/banner2.png'
 
 const Home = () => {
     return (
         <div>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner1}
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner2}
-                        alt="Second slide"
-                    />
-                </Carousel.Item>
-            </Carousel>
+            <Slider></Slider>
         </div>
     );
 };
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import banner1 from '../../Images/Banner/banner1.png'
+import banner2 from '../../Images/Banner/banner2.png'
+
+const Slider = () => {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Slider;
